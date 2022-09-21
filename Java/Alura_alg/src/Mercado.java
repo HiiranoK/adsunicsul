d68418534c09ad9376333d015f6e4b6d8c3c09e2
Richard Cabral <richardcabraloliveira@gmail.com>
2022-09-21 16:39:03 -0300

    enviando arquivos para o git via vscode.

--- Java/Alura_alg/src/Mercado.java
+++ Java/Alura_alg/src/Mercado.java
@@ -9,7 +9,14 @@ public class Mercado {
             "uva",
             "melao"
         };
+        String[] frutas_cliente= new String[5];
         Scanner sc = new Scanner(System.in);
+        for(int i=0;i>=frutas_cliente.length;i++){
+            System.out.println("Informe a"+ i+1 + "ª fruta da sua lista");
+            String fruta = sc.nextLine();
+            frutas_cliente[i]= fruta;
+        }
+        System.out.println(frutas_cliente);
         System.out.println("Informe uma fruta");
         String fruta = sc.nextLine();
         sc.close();
@@ -17,7 +24,7 @@ public class Mercado {
         System.out.println(fruta);
         boolean lista = false;
         for(int i = 0; i < frutas.length; i++){
-            if(fruta.equals(frutas[i])){
+            if(fruta.equalsIgnoreCase(frutas[i])){
                 lista = true;
             }
         }
---
+++ python/LetsCode/exercicios/teste.py
@@ -0,0 +1,17 @@
+i = 85
+j = 115
+incremento = 7
+incremento2 = 8
+valor = 0
+valor2=0
+while(i <= 1500):
+    i+= incremento
+    if(i>= 1000): 
+        valor+=1
+print(valor)
+while(j <= 1500):
+    j+= incremento2
+    if(j>= 1000): 
+        valor+=1
+print(valor2)
+print(valor+valor2)
